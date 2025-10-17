c35f7d8fe47782c27a99e8a78add82178c11c97d
mahmoud5704eng <mhnh5704@gmail.com>
2025-10-17 23:49:20 +0300

    Create CustomerProduct.java

---
+++ CustomerProduct.java
@@ -0,0 +1,47 @@
+package tasks.absract_database;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class CustomerProduct implements Record {
+
+    private String customerSSN;
+    private String productID;
+    private LocalDate purchaseDate;
+    private boolean paid;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate;
+    }
+
+    public String getCustomerSSN() {
+        return customerSSN;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    public String lineRepresentation() {
+        return customerSSN + "," + productID + "," + purchaseDate.format(formatter) + "," + paid;
+    }
+
+    public String getSearchKey() {
+        return customerSSN + "," + productID+","+purchaseDate.format(formatter);
+    }
+}
