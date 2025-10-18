4b733cbf37b9b307fd556f4a6cb73dd8b1aa720c
MahmoudMohammedShaheen <mahmoudshaheen755@gmail.com>
2025-10-18 11:00:52 +0300

    Update AdminRole.java

--- AdminRole.java
+++ AdminRole.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 
 import java.util.ArrayList;
@@ -52,6 +49,7 @@ public class AdminRole
         database.saveToFile();
     }
 }
+
 
 
 
