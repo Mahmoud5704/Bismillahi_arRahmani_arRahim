da0ab287aa35796febd70e0639024cf6369a1085
Mahmoud5704 <mhnh5704@gmail.com>
2025-10-20 01:45:53 +0300

    Delete full_project directory

--- full_project/interface_Record.java
+++
@@ -1,5 +0,0 @@
-package full_project;
-public interface interface_Record {
-   String lineRepresentation();
-   String getSearchKey();
-}
--- full_project/interface_UserRole.java
+++
@@ -1,4 +0,0 @@
-package full_project;
-public interface interface_UserRole {
-     void logout();
-}
