715775f49d39a7f7ff8ca72959dccd5993ea5ec8
mahmoud5704eng <mhnh5704@gmail.com>
2025-10-19 03:33:21 +0300

    clean something from database,delete some prints
    dfgdg

---
+++ full_project/CustomerProductDatabase.java
@@ -0,0 +1,19 @@
+package full_project;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class CustomerProductDatabase extends Abstrat_FileDatabase<CustomerProduct> {
+    public CustomerProductDatabase(String filename) {
+        super(filename);
+    }
+
+    @Override
+    public CustomerProduct createRecordFrom(String line) {
+        String[] parts = line.trim().split(",");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            LocalDate date = LocalDate.parse(parts[2], formatter);
+            CustomerProduct obj1 = new CustomerProduct(parts[0], parts[1], date);
+            obj1.setPaid(Boolean.parseBoolean(parts[3]));
+            return obj1;
+    }
+}
